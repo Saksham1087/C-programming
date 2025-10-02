abc4690d1e099befc256038b0b8cd026c87d729f
Saksham1087 <raisaksham1087@gmail.com>
2025-10-02 10:56:16 +0530

    2nd area of rectangle function

---
+++ c-programs/AreaOfRectCalc2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a= 15;  // length of a rectangle 
+    int b= 30;  // breadth of a rectangle 
+    int c=a*b;  // area of a rectangle
+    
+    printf("length of a rectangle is %d unit\n",a);
+    printf("breadth of a rectangle is %d unit\n\n",b);
+    printf("The area of rectangle is %d sq.unit",c);
+    return 0;
+}
